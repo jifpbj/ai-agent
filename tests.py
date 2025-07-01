5d8e35fbddf5f90959d8a5675b01cd11455cfb4e
Jiff <102629485+jifpbj@users.noreply.github.com>
2025-07-01 10:31:13 -0700

    added test for get_file_content

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+
 """
 print(get_files_info("calculator", "."))
 print(get_files_info("calculator", "pkg"))
